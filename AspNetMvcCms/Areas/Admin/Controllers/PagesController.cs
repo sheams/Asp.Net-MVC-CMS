c76ed2b212fd0b70306b75436e0b7764f23139fd
sheams <sheams.net@gmail.com>
2015-11-20 23:12:31 +0600

    Added page creation codes.

---
+++ AspNetMvcCms/Areas/Admin/Controllers/PagesController.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using AspNetMvcCms.Models;
+using Microsoft.AspNet.Identity;
+using System.Text;
+
+namespace AspNetMvcCms.Areas.Admin.Controllers
+{
+    public class PagesController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: Admin/Pages
+        public ActionResult Index()
+        {
+            return View(db.Pages.ToList());
+        }
+
+        // GET: Admin/Pages/Details/5
+        public ActionResult Details(long? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Page page = db.Pages.Find(id);
+            if (page == null)
+            {
+                return HttpNotFound();
+            }
+            return View(page);
+        }
+
+        // GET: Admin/Pages/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Admin/Pages/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        [ValidateInput(false)]
+        public ActionResult Create([Bind(Include = "Id,Title,MetaKeyword,MetaDescription,ShortDescription,Body,Status")] Page page, string bodyText)
+        {
+            if (ModelState.IsValid)
+            {
+                var userId = User.Identity.GetUserId();
+                page.CreateBy = userId;
+                page.ModifyBy = userId;
+                page.CreationDate = DateTime.Now;
+                page.ModificationDate = DateTime.Now;
+                var htmBody = Encoding.UTF8.GetBytes(bodyText);
+                page.Body = htmBody;
+
+                db.Pages.Add(page);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(page);
+        }
+
+        // GET: Admin/Pages/Edit/5
+        public ActionResult Edit(long? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Page page = db.Pages.Find(id);
+            if (page == null)
+            {
+                return HttpNotFound();
+            }
+            return View(page);
+        }
+
+        // POST: Admin/Pages/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Title,MetaKeyword,MetaDescription,ShortDescription,Body,CreationDate,ModificationDate,CreateBy,ModifyBy,Status")] Page page)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(page).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(page);
+        }
+
+        // GET: Admin/Pages/Delete/5
+        public ActionResult Delete(long? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Page page = db.Pages.Find(id);
+            if (page == null)
+            {
+                return HttpNotFound();
+            }
+            return View(page);
+        }
+
+        // POST: Admin/Pages/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(long id)
+        {
+            Page page = db.Pages.Find(id);
+            db.Pages.Remove(page);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ AspNetMvcCms/Controllers/PageController.cs
@@ -0,0 +1,100 @@
+﻿using AspNetMvcCms.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AspNetMvcCms.Controllers
+{
+    public class PageController : Controller
+    {
+        ApplicationDbContext _db = new ApplicationDbContext();
+        // GET: Page
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: Page/Details/5
+        public ActionResult Details(int id)
+        {
+            var page = _db.Pages.Find(id);
+
+            if (page != null)
+            {
+                ViewBag.MetaDescription = page.MetaDescription;
+                ViewBag.MetaKeyword = page.MetaKeyword;
+                ViewBag.BodyText = Encoding.UTF8.GetString(page.Body);
+            }
+            return View(page);
+        }
+
+        // GET: Page/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Page/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Page/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Page/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Page/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Page/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
